c29c17e0d9fad9185999c216aa7e3c6c9acbbfef
Seamile <lanhuermao@gmail.com>
2023-05-09 23:25:34 +0800

    add test case

---
+++ test.py
@@ -0,0 +1,49 @@
+import json
+import tempfile
+import unittest
+from jsonfmt import (parse_jsonpath, get_element_by_components,
+                     jsonpath_match, output)
+
+
+class JSONFormatToolTestCase(unittest.TestCase):
+    def setUp(self):
+        with open('example.json') as json_fp:
+            self.example_json = json.load(json_fp)
+
+    def test_parse_jsonpath(self):
+        jsonpath = "history/0/items/1/bar"
+        expected_components = ["history", 0, "items", 1, "bar"]
+        components = parse_jsonpath(jsonpath)
+        self.assertEqual(components, expected_components)
+
+    def test_get_element_by_components(self):
+        components = ["history", 0, "items", 1, "bar"]
+        expected_element = -222
+        element = get_element_by_components(self.example_json, components)
+        self.assertEqual(element, expected_element)
+
+    def test_jsonpath_match(self):
+        jsonpath = "history/*/items/1/foo"
+        expected_matched_obj = [-111, -333, -555]
+        matched_obj = jsonpath_match(self.example_json, jsonpath)
+        self.assertEqual(matched_obj, expected_matched_obj)
+
+    def test_output(self):
+        json_obj = {"name": "John", "age": 30}
+        expected_output = '{"age":30,"name":"John"}\n'
+
+        # Create a temporary file
+        with tempfile.NamedTemporaryFile(mode='r+') as temp_file:
+            # Call the output function with the temporary file
+            output(json_obj, True, False, 4, temp_file)
+
+            # Read the content of the temporary file
+            temp_file.seek(0)
+            output_str = temp_file.read()
+
+            # Check the output content
+            self.assertEqual(output_str, expected_output)
+
+
+if __name__ == '__main__':
+    unittest.main()
