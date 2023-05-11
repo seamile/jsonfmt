25321df66b84f30c7a9aa3e721ba365ef6e42c11
Seamile <lanhuermao@gmail.com>
2023-05-11 20:34:20 +0800

    update test

--- test.py
+++ test.py
@@ -11,20 +11,20 @@ class JSONFormatToolTestCase(unittest.TestCase):
             self.example_json = json.load(json_fp)
 
     def test_parse_jsonpath(self):
-        jsonpath = "history/0/items/1/bar"
-        expected_components = ["history", 0, "items", 1, "bar"]
+        jsonpath = "history/0/items/2/name"
+        expected_components = ["history", 0, "items", 2, "name"]
         components = parse_jsonpath(jsonpath)
         self.assertEqual(components, expected_components)
 
     def test_get_element_by_components(self):
-        components = ["history", 0, "items", 1, "bar"]
-        expected_element = -222
+        components = ["history", 0, "items", 1, "calorie"]
+        expected_element = 266
         element = get_element_by_components(self.example_json, components)
         self.assertEqual(element, expected_element)
 
     def test_jsonpath_match(self):
-        jsonpath = "history/*/items/1/foo"
-        expected_matched_obj = [-111, -333, -555]
+        jsonpath = "history/*/items/1/calorie"
+        expected_matched_obj = [266, 54.5, -350]
         matched_obj = jsonpath_match(self.example_json, jsonpath)
         self.assertEqual(matched_obj, expected_matched_obj)
 
