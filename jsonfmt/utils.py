87d3a32b9b357ed1046a67b30a2ed972880ce1f6
Seamile <lanhuermao@gmail.com>
2023-08-16 14:43:33 +0800

    add dict_to_xml

--- jsonfmt/utils.py
+++ jsonfmt/utils.py
@@ -1,10 +1,12 @@
 import re
+from sys import stderr
+from typing import Any
 
 NUMERIC = re.compile(r'-?\d+$|-?\d+\.\d+$|^-?\d+\.?\d+e-?\d+$')
 DICT_OR_LIST = re.compile(r'^\{.*\}$|^\[.*\]$')
 
 
-def load_value(value: str):
+def load_value(value: str) -> Any:
     if NUMERIC.match(value):
         return eval(value)
     elif DICT_OR_LIST.match(value):
@@ -14,3 +16,11 @@ def load_value(value: str):
             return value
     else:
         return value
+
+
+def print_inf(msg: Any):
+    print(f'\033[1;94mjsonfmt:\033[0m \033[0;94m{msg}\033[0m', file=stderr)
+
+
+def print_err(msg: Any):
+    print(f'\033[1;91mjsonfmt:\033[0m \033[0;91m{msg}\033[0m', file=stderr)
